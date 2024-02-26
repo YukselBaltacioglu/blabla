0b24f923b843d2099501a90b24b7041c71c6371e
mrvOzt <mervemr2601@gmail.com>
2024-02-26 12:10:52 +0300

    update

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def Defination():
-    return "flask denemesi"
-
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+ 
+    # Returning an api for showing in  reactjs
+    return {
+        'name':"Marka", 
+        'logo':"https://petsurfer.com/blog/wp-content/uploads/kedi-sahiplenme.png",
+        'brandName' : "Benzerlik"
+        }
 
 if __name__ == "__main__":
     app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
@@ -6,21 +6,22 @@ import Button from "./components/Button/Button";
 import Search from "./components/Search/Search";
 import "./App.css";
 
-
 const App = () => {
-  const [data,setData]= useState([{}])
+  const [data, setData] = useState({ name: "" });
 
-  useEffect ( () => {
-    fetch("/").then ( 
-      res=>  res.json()
-    ).then(
-      data=> {
-        setData(data)
-        console.log(data)
-      }
-    )
-
-  }, [])
+  // Using useEffect for single rendering
+  useEffect(() => {
+    fetch("/data")
+      .then((res) => res.json())
+      .then((apiData) => {
+        // Setting data from the API
+        setData({
+          name: apiData.name,
+          brandName: apiData.brandName,
+          logo: apiData.logo,
+        });
+      });
+  }, []);
 
   return (
     <Router>
@@ -30,12 +31,12 @@ const App = () => {
         </div>
         <div className="content" style={{ display: "flex" }}>
           <Sidebar />
-          <Button />
-          <Button />
-          <Button />  
+          <Button name={data.name} brandName={data.brandName} logo={data.logo}/>
+          <Button name={data.name} brandName={data.brandName} logo={data.logo}/>
+          <Button name={data.name} brandName={data.brandName} logo={data.logo}/>
         </div>
-        <div > 
-          <Search /> 
+        <div>
+          <Search />
         </div>
       </div>
     </Router>
